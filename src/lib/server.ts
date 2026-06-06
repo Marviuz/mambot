715ec3f90a3b47b7ff77671d0ae956350132dead
Marviuz <marviuz.dev@gmail.com>
2026-06-06 18:56:43 +0800

    hotfix: healthcheck method

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -14,7 +14,10 @@ export class HttpServer {
   constructor() {
     this.port = env.PORT ?? 8000;
     this.server = createServer((req, res) => {
-      if (req.method === 'GET' && req.url === '/_/healthcheck') {
+      if (
+        (req.method === 'GET' || req.method === 'HEAD') &&
+        req.url === '/_/healthcheck'
+      ) {
         logger.info('Healthcheck');
         res.writeHead(200, { 'content-type': 'application/json' });
         res.end(JSON.stringify({ status: 'ok' }));
